bc094a0b64e0c1ffd6dc4764cf239c9b182abaff
nihar078 <nihar078@gmail.com>
2023-08-01 20:33:45 +0530

    task5_Implement the Home page

---
+++ src/components/Home/TourItem.js
@@ -0,0 +1,33 @@
+import { Button, Col, Container, Row } from "react-bootstrap";
+import "./TourItem.css";
+
+const TourItem = (props) => {
+  return (
+    <Container>
+      <Row
+        style={{
+          borderBottom: "1px solid black",
+          padding: "5px",
+          fontFamily: "-moz-initial",
+          textAlign: "center"
+        }}
+      >
+        <Col md={2}>{props.date}</Col>
+        <Col md={3} style={{ color: "grey" }}>
+          {props.place}
+        </Col>
+        <Col md={4} style={{color: "grey"}}>{props.spec_place}</Col>
+        <Col md={3} className="buy-tickets">
+          <Button
+            variant="info"
+            style={{ color: "white", fontWeight: "bold",}}
+          >
+            BUY TICKETS
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default TourItem;
